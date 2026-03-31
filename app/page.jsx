4918aeea2904645e521a1860929b3803ede3cccf
VishwaTej26 <codelearnearn15lpa@gmail.com>
2026-03-31 20:25:18 +0530

    Add Traffic AI System homepage with TrafficCanvas

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import TrafficCanvas from "../components/TrafficCanvas";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Traffic AI System</h1>
+      <TrafficCanvas />
+    </div>
+  );
+}
